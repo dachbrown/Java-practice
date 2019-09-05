0dc3a025b8f574b277fa33aa6b3e37155e46b1a2
dbrow208 <dbrow208@a23-202-231-169.deploy.static.akamaitechnologies.com>
2019-09-05 19:54:55 -0400

    Finished basic homework.

--- src/lab2/Lab_Assignment_2.java
+++ src/lab2/Lab_Assignment_2.java
@@ -10,27 +10,33 @@ public class Lab_Assignment_2
 		long end_time = start_time + 30000;
 		long current_time = System.currentTimeMillis();
 		int current_score = 0;
-		boolean end_while = false;
+		boolean exit_while_loop = false;
 		Random random = new Random();
-		while(current_time < end_time && end_while == false)
+		while(current_time < end_time && exit_while_loop == false)
 		{
 			int r = random.nextInt(20);
 			String test_aa = FULL_NAMES[r];
 			System.out.println("What is the one letter code for " + test_aa + "?");
 			String user_answer = System.console().readLine().toUpperCase();
-			if( SHORT_NAMES[r].equals(user_answer))
+			if( SHORT_NAMES[r].equals(user_answer) == true)
 			{
 				current_score++;
 				System.out.println("Correct! Score: " + current_score + "; Time Remaining: " + (end_time - current_time)/1000f + " seconds.");
 			}
+			else if( SHORT_NAMES[r].equals(user_answer) == false)
+			{
+				exit_while_loop = true;
+				System.out.println("Sorry, that was incorrect. The correct answer is : " + SHORT_NAMES[r] + ". Final score: " + current_score);
+				
+			}
 			else
 			{
-				end_while = true;
-				System.out.println("Sorry, the test has ended. Final score: " + current_score);
-				
+				exit_while_loop = true;
+				System.out.println("Time is up!");
 			}
 			current_time = System.currentTimeMillis();
 		}
+		System.out.println("The test has ended.");
 	};
 	public static String[] SHORT_NAMES = 
 	{
